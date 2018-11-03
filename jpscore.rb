271e6a896c6d3ded31fd7443c36b8c56e0bc2194
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2018-11-03 19:05:31 +0100

    add firmula

---
+++ jpscore.rb
@@ -0,0 +1,22 @@
+class Jpseditor < Formula
+  desc "Core simulation module for JuPedSim"
+  homepage "https://www.jupedsim.org/"
+
+  head "https://github.com/JuPedSim/jpscore.git"
+  # options
+  # AIROUTER --> CGAL
+  # JPSFIRE --> ZLIB
+  # BUILD_TESTING
+
+  depends_on "cmake" => :build
+  depends_on "boost"
+  # timer chrono system filesystem unit_test_framework
+
+  def install
+    Dir.pwd
+    Dir.mkdir "build"
+    Dir.chdir "build"
+    system "cmake", ".."
+    system "make", "install"
+  end
+end
---
+++ jpsvis.rb
@@ -0,0 +1,23 @@
+class Jpseditor < Formula
+  desc "Visualisation module for JuPedSim"
+  homepage "https://www.jupedsim.org/"
+
+  head "https://github.com/JuPedSim/jpsvis.git"
+
+  depends_on "cmake" => :build
+  depends_on "qt"
+  depends_on "vtk"
+
+  def install
+    Dir.pwd
+    Dir.mkdir "build"
+    Dir.chdir "build"
+    system "cmake", ".."
+    system "make", "install"
+    system("cp /tmp/jpsvis.app/Contents/MacOS/jpsvis /usr/local/bin")
+    puts ""
+    puts "------------------------------------"
+    puts "JPSvis installed in /usr/local/bin/jpsvis"
+    puts "------------------------------------"
+  end
+end
