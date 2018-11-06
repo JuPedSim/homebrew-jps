839dfe978e7816b9466b808ec0d1e60dc77ae1a4
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2018-11-06 12:27:57 +0100

    debug jpsvis

--- jpsvis.rb
+++ jpsvis.rb
@@ -4,21 +4,29 @@ class Jpsvis < Formula
 
   head "https://github.com/JuPedSim/jpsvis.git"
 
+  option "with-demos", "Add demo files"
+
   depends_on "cmake" => :build
   depends_on "qt"
   depends_on "vtk"
 
   def install
-    Dir.pwd
+    ohai "#{prefix}"
+    ohai "#{bin}"
+    ohai "#{buildpath}"
     Dir.mkdir "build"
     Dir.chdir "build"
     system "cmake", ".."
     system "make", "install"
-    bin.install "#{prefix}/JPSeditor.app/Contents/MacOS/jpsvis"
+    bin.install "#{prefix}/jpsvis.app/Contents/MacOS/jpsvis"
     prefix.install "#{prefix}/jpsvis.app"
-
     ohai "jpsvis install in #{prefix}/jpsvis.app"
     ohai ".. and linked to #{bin}"
+    if build.with? "demos"
+      doc.install Dir["#{buildpath}/samples"]
+      ohai "Additional demo files are installed in #{doc}"
+    end
+
 
   end
 end
