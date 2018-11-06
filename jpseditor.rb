e0ca8cfe212565d40ea02e578a21705686c304c9
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2018-11-06 12:20:41 +0100

    add with demos option to jpseditor

--- jpseditor.rb
+++ jpseditor.rb
@@ -4,17 +4,20 @@ class Jpseditor < Formula
 
   head "https://github.com/JuPedSim/jpseditor.git"
 
+  option "with-demos", "Add demo files"
   depends_on "qt"
 
   def install
-    ohai "#{buildpath}"
-    ohai "#{prefix}"
-    ohai "#{bin}"
     system "qmake", "."
     system "make", "install"
     bin.install "#{buildpath}/build/release/JPSeditor.app/Contents/MacOS/JPSeditor"
     prefix.install "#{buildpath}/build/release/JPSeditor.app"
-    ohai "jpseditor install in #{prefix}/JPSeditor.app"
+    ohai "jpseditor installed in #{prefix}/JPSeditor.app"
     ohai ".. and linked to #{bin}"
+    if build.with? "demos"
+      doc.install Dir["#{buildpath}/examples"]
+      ohai "Additional demo files are installed in #{doc}"
+    end
+
   end
 end
